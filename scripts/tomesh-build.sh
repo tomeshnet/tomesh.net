9567b10a18a87b56ba65e48914b479fe67d7b782
Garry Ing <garry@format.com>
2016-09-24 16:11:21 -0400

    build script update

--- scripts/tomesh-build.sh
+++ scripts/tomesh-build.sh
@@ -8,4 +8,4 @@ set -e
 site="/var/www/tomesh.net/html"
 source="/var/www/jekyll-hook/tomeshnet/tomesh.net/master/code"
 
-/usr/local/bin/jekyll build -s $source -d $site
+/usr/bin/jekyll build -s $source -d $site
