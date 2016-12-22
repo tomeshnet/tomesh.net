5060a5c91f33c36dc24b6f802af251647d2ffd3f
dcwalk <dcwalk@users.noreply.github.com>
2016-12-21 22:12:30 -0500

    change button text when show more toggled

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ var eventToggle = document.getElementById('event-toggle')
 var showMore = document.getElementById('show-more')
 
 eventToggle.addEventListener('click', function () {
+  if (eventToggle.getAttribute('data-text-swap') == eventToggle.innerHTML) {
+    eventToggle.innerHTML = eventToggle.getAttribute('data-text-original')
+  } else {
+    eventToggle.setAttribute('data-text-original', eventToggle.innerHTML)
+    eventToggle.innerHTML = eventToggle.getAttribute('data-text-swap')
+  }
   showMore.classList.toggle('show')
 }, false)
