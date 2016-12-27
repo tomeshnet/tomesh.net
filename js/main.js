157957c72685b70ad6a0d27619dee2d76edb49e2
Garry Ing <garry@format.com>
2016-12-26 21:00:26 -0500

    dom exist condition

--- js/main.js
+++ js/main.js
@@ -29,16 +29,18 @@ window.addEventListener('scroll', callbackFunc);
 //
 var eventToggle = document.getElementById('event-toggle')
 var showMore = document.getElementById('show-more')
+if (eventToggle) {
+  eventToggle.addEventListener('click', function () {
+    var state = eventToggle.getAttribute('aria-expanded') === 'false' ? true : false;
+    if (eventToggle.getAttribute('data-text-swap') == eventToggle.innerHTML) {
+      eventToggle.innerHTML = eventToggle.getAttribute('data-text-original')
+    } else {
+      eventToggle.setAttribute('data-text-original', eventToggle.innerHTML)
+      eventToggle.innerHTML = eventToggle.getAttribute('data-text-swap')
+    }
+    eventToggle.setAttribute('aria-expanded', state);
+    showMore.setAttribute('aria-hidden', !state);
+    showMore.classList.toggle('show')
+  }, false)
+}
 
-eventToggle.addEventListener('click', function () {
-  var state = eventToggle.getAttribute('aria-expanded') === 'false' ? true : false;
-  if (eventToggle.getAttribute('data-text-swap') == eventToggle.innerHTML) {
-    eventToggle.innerHTML = eventToggle.getAttribute('data-text-original')
-  } else {
-    eventToggle.setAttribute('data-text-original', eventToggle.innerHTML)
-    eventToggle.innerHTML = eventToggle.getAttribute('data-text-swap')
-  }
-  eventToggle.setAttribute('aria-expanded', state);
-  showMore.setAttribute('aria-hidden', !state);
-  showMore.classList.toggle('show')
-}, false)
