462eb55f23e45f7f23097c85504598534c809d26
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-04-27 08:11:04 -0400

    Add Theme to map (#203)
    * Add theme to map
    * Added on/off toggle for Buildings and Features (disable/enable theme)

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@ var infowindow = null;
 var markers = [];
 var currentNodeListURL;
 var circle = null;
+var mapStyle;
 
 function initialize() {
   //Current Node URL with random bits to make sure it doesnt get cached
@@ -12,7 +13,245 @@ function initialize() {
   var filterActive = document.getElementById('chkActive').checked;
   var filterProposed = document.getElementById('chkProposed').checked;
   var zoomGroup = document.getElementById('chkGroup').checked;
+  var filterBuildings = document.getElementById('chkTheme').checked;
 
+  //mapStyling from https://mapstyle.withgoogle.com/
+  mapStyle = [
+    {
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'weight': '2.00'
+        }
+      ]
+    },
+    {
+      'elementType': 'geometry.stroke',
+      'stylers': [
+        {
+          'color': '#9c9c9c'
+        }
+      ]
+    },
+    {
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'color': '#0e093f'
+        },
+        {
+          'saturation': '0'
+        },
+        {
+          'lightness': '0'
+        },
+        {
+          'visibility': 'on'
+        },
+        {
+          'weight': '0.4'
+        }
+      ]
+    },
+    {
+      'featureType': 'administrative',
+      'elementType': 'geometry',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'administrative.land_parcel',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'administrative.neighborhood',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'landscape',
+      'stylers': [
+        {
+          'color': '#f2f2f2'
+        }
+      ]
+    },
+    {
+      'featureType': 'landscape',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#fdfffc'
+        }
+      ]
+    },
+    {
+      'featureType': 'landscape.man_made',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#fdfffc'
+        }
+      ]
+    },
+    {
+      'featureType': 'poi',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'stylers': [
+        {
+          'saturation': -100
+        },
+        {
+          'lightness': 45
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#dde9e3'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'labels.icon',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'labels.text.fill',
+      'stylers': [
+        {
+          'color': '#7b7b7b'
+        }
+      ]
+    },
+    {
+      'featureType': 'road',
+      'elementType': 'labels.text.stroke',
+      'stylers': [
+        {
+          'color': '#ffffff'
+        }
+      ]
+    },
+    {
+      'featureType': 'road.arterial',
+      'elementType': 'labels.icon',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'road.highway',
+      'stylers': [
+        {
+          'visibility': 'simplified'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit',
+      'stylers': [
+        {
+          'visibility': 'off'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.line',
+      'elementType': 'labels',
+      'stylers': [
+        {
+          'saturation': '0'
+        },
+        {
+          'lightness': '0'
+        }
+      ]
+    },
+    {
+      'featureType': 'transit.line',
+      'elementType': 'labels.text',
+      'stylers': [
+        {
+          'weight': '0.51'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'stylers': [
+        {
+          'color': '#46bcec'
+        },
+        {
+          'visibility': 'on'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'geometry.fill',
+      'stylers': [
+        {
+          'color': '#c4dfed'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'labels.text.fill',
+      'stylers': [
+        {
+          'color': '#070707'
+        }
+      ]
+    },
+    {
+      'featureType': 'water',
+      'elementType': 'labels.text.stroke',
+      'stylers': [
+        {
+          'color': '#ffffff'
+        }
+      ]
+    }
+  ];
   //Prepare default view and create map
   var mapOptions = {
     zoom: 12,
@@ -30,7 +269,11 @@ function initialize() {
   });
 
   map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+  //Store default theme
 
+  if (filterBuildings == false) {
+    map.setOptions({styles: mapStyle});
+  }
   //Reset markers array
   markers = undefined;
   markers = [];
@@ -377,6 +620,5 @@ function optionExpand() {
     $('#mapOptions').addClass('FullHeight');
   }
 }
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
