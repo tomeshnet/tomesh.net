ec47256aaffdb37ac365d23439e09806a656700b
Nicolas Pace <nicopace@gmail.com>
2020-06-09 23:39:15 -0300

    Fixes to codeclimate checks.

--- js/map.js
+++ js/map.js
@@ -173,28 +173,28 @@ function addMarker(map, nodeResult, name, location) {
     var x = 16;
     var y = 16;
     switch (ArrowDirection) {
-      case 'North':
-      case 'North East':
-      case 'North West':
-        y = 32;
-        break;
-      case 'South':
-      case 'South East':
-      case 'South West':
-        y = 0;
-        break;
+    case 'North':
+    case 'North East':
+    case 'North West':
+      y = 32;
+      break;
+    case 'South':
+    case 'South East':
+    case 'South West':
+      y = 0;
+      break;
     }
     switch (ArrowDirection) {
-      case 'East':
-      case 'North East':
-      case 'South East':
-        x = 0;
-        break;
-      case 'West':
-      case 'North West':
-      case 'South West':
-        x = 32;
-        break;
+    case 'East':
+    case 'North East':
+    case 'South East':
+      x = 0;
+      break;
+    case 'West':
+    case 'North West':
+    case 'South West':
+      x = 32;
+      break;
     }
 
     var imageAnchor = new google.maps.Point(x, y);
@@ -410,5 +410,26 @@ function toggleClass(toggleID, toggleClass) {
     $('#' + toggleID).addClass(toggleClass);
   }
 }
+function drawArc(center, initialBearing, finalBearing, radius) {
+  var d2r = Math.PI / 180;   // degrees to radians 
+  var r2d = 180 / Math.PI;   // radians to degrees 
+
+  var points = 32;
+
+  // find the raidus in lat/lon 
+  var rlat = (radius / EarthRadiusMeters) * r2d;
+  var rlng = rlat / Math.cos(center.lat() * d2r);
+
+  var extp = new Array();
+
+  if (initialBearing > finalBearing) finalBearing += 360;
+  var deltaBearing = finalBearing - initialBearing;
+  deltaBearing = deltaBearing / points;
+  extp.push(center);
+  for (var i = 0; (i < points + 1); i++) {
+    extp.push(center.DestinationPoint(initialBearing + i * deltaBearing, radius));
+  }
+  return extp;
+} 
 
 google.maps.event.addDomListener(window, 'load', initialize);
