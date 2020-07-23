788d2fd8f452929e5b85b24443e0f5659fd278ff
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-07-23 17:43:33 -0400

    More code climate happiness

--- js/map.js
+++ js/map.js
@@ -5,8 +5,6 @@ var currentNodeListURL;
 var circle = null;
 var mapStyle;
 
-var EarthRadiusMeters = 6378137.0; // meters
-
 
 function initialize() {
   //Current Node URL with random bits to make sure it doesnt get cached
@@ -383,15 +381,8 @@ function toggleClass(toggleID, toggleClass) {
   }
 }
 function drawArc(center, initialBearing, finalBearing, radius) {
-//  var d2r = Math.PI / 180;   // degrees to radians 
-  var r2d = 180 / Math.PI;   // radians to degrees 
 
   var points = 32;
-
-  // find the raidus in lat/lon 
-//  var rlat = (radius / EarthRadiusMeters) * r2d;
-  //var rlng = rlat / Math.cos(center.lat() * d2r);
-
   var extp = new Array();
 
   if (initialBearing > finalBearing) finalBearing += 360;
